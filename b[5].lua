c10396cb3ab103f3d6199d99e140d52304fb836c
Arie_dayo <157713538+Ariedaiko@users.noreply.github.com>
2025-03-02 22:45:34 +0900

    Update b[5].lua

--- b[5].lua
+++ b[5].lua
@@ -4,7 +4,7 @@ local v = gg.getResults(1,3)[1].address
 gg.clearResults()
 gg.searchNumber('-257~256;'..('-257~~256;'):rep(4)..'-257~256::31',4,false,2^29,v,v+0xfff)
 gg.refineNumber('-257~~256',4)
-if #v <= 0 then
+if gg.getResultsCount() <= 0 then
   x.r('ms1', 'Ticket1[1]')
 end
 return gg.getResults(4,gg.getResultsCount()-4)
