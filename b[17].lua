aa490c3c56313232bd307bfcfb7abf5c57e71d9f
Arie_dayo <157713538+Ariedaiko@users.noreply.github.com>
2025-03-08 21:52:25 +0900

    Update b[17].lua

--- b[17].lua
+++ b[17].lua
@@ -1,13 +1,20 @@
 gg.clearResults()
-gg.searchNumber('h 00 00 00 00 64 00 00 00 00 00 00 00',4,false,2^29,base-0xD000,base-0x6000)
+gg.searchNumber('h E8 03 00 00 10 0E 00 00 E8 03 00 00',4,false,2^29,base-0xD000,base-0x6000)
+gg.refineNumber('16',1)
 local res = gg.getResults(gg.getResultsCount())
 if #res<=0 then
   x.r('m4','b[17]_1')
 end
-gg.clearResults()
-gg.searchNumber(('-257~256;'):rep(3)..('-257~~256;'):rep(3)..('-257~~256::25'),4,false,2^29,res[1].address-0x999,res[1].address)
-local res = gg.getResults(gg.getResultsCount())
-if #res<=0 then
+for i = 1, #res do
+  gg.clearResults()
+  gg.searchNumber(('-257~256;'):rep(3)..('-257~~256;'):rep(3)..('-257~~256::25'),4,false,2^29,res[i].address-0x7ff,res[i].address-0x300)
+  local v = gg.getResults(gg.getResultsCount())
+  if #v >= 1 then
+    break
+  end
+end
+if #v<=0 then
   x.r('m4','b[17]_2')
 end
-return res[1]
+return v[1]
+
